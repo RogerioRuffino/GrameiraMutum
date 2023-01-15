42df3e3cf59de98074f12fc57385ab97659feb9d
ROGERIO RUFINO <92214783+RogerioRuffino@users.noreply.github.com>
2023-01-15 09:17:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,73 @@
 const preco = document.getElementById('preco');
-let valor =  document.getElementById('valor');
 
 let input = document.getElementById('input');
-
 let result = document.getElementById('result');
+
 let inputType = document.getElementById('inputType');
 let resultType = document.getElementById('resultType');
+
+let display1 = document.getElementById('div1').style.display;
+
+let display2 = document.getElementById('div2').style.display;
 
 
 // now addEventListener
 preco.addEventListener('keyup', myResult);
-preco.addEventListener('keyup', myResult);
 input.addEventListener('keyup',myResult);
 inputType.addEventListener('change',myResult);
 resultType.addEventListener('change',myResult);
-// console.log(input);
+
 // taking initial values
 option_from = inputType.value;
 option_to = resultType.value;
 option_to = resultapetes.value;
 option_to = valor.value;
+option_to = m2.value;
 
+ 	
 
 
 
 function myResult(){
     option_from = inputType.value;
     option_to = resultType.value;
-	
 
-  
     if(option_from === "m2" && option_to==="pal"){
+
+		document.getElementById('div1').style.display = 'block';
+       		document.getElementById('div2').style.display = 'none';
+
 		result.value = (Math.floor(Number(input.value) / 50));
+
 		resultapetes.value = (Number(input.value) % 50*4);
+
 		valor.value = input.value * Number(preco.value);
+		
+		m2.value = Number(input.value);
+		
 	} 
 
 	//===================================================
 
-    if(option_from === "R$" && option_to==="pal"){
+	if(option_from === "R$" && option_to==="pal"){
+		document.getElementById('div2').style.display = 'block';
+    		document.getElementById('div1').style.display = 'none';
+
 		result.value = (Math.floor((Number(input.value)) / (50*Number(preco.value))));
 		resultapetes.value = (Math.floor((Number(input.value)) % (50*Number(preco.value))))/ Number(preco.value)*4;
 		valor.value = Number(input.value);
-	}else if(option_from === "R$" && option_to==="tapetes"){
+		m2.value = Number(input.value)/12;
+		
 
-		//this is m2 to R$ formula 
-		result.value = Number(input.value)/(50*Number(preco.value));
 
-	}else if(option_from === "R$" && option_to==="R$"){
-		//this is m2 to m2 formula 
-		result.value =  Number(input.value);
-	}else if(option_from === "R$" && option_to==="pal"){
-		//this is m2 to m2 formula 
-		result.value = Number(input.value)/600;
 	}
-
-	// //===================================================
-
-    // if(option_from === "tapetes" && option_to==="m2"){
-	// 	//this is tapetes to tapetes formula 
-	// 	result.value = Number(input.value)/4;
-	// }else if(option_from === "tapetes" && option_to==="tapetes"){
-
-	// 	//this is m2 to R$ formula 
-	// 	result.value = Number(input.value);
-
-	// }else if(option_from === "tapetes" && option_to==="R$"){
-	// 	//this is m2 to m2 formula 
-	// 	result.value = input.value * 3;
-	// }else if(option_from === "tapetes" && option_to==="pal"){
-	// 	//this is m2 to m2 formula 
-	// 	result.value = Number(input.value) / 200;
+	//else if(option_from === "R$" && option_to==="tapetes"){
+	// 	result.value = Number(input.value)/(50*Number(preco.value));
+	// }else if(option_from === "R$" && option_to==="R$"){
+	// 	result.value =  Number(input.value);
+	// }else if(option_from === "R$" && option_to==="pal"){
+	// 	result.value = Number(input.value)/600;
 	// }
-
-	// //===================================================
-
-    // if(option_from === "pal" && option_to==="m2"){
-	// 	//this is tapetes to tapetes formula 
-	// 	result.value = Number(input.value)*50;
-	// }else if(option_from === "pal" && option_to==="tapetes"){
-
-	// 	//this is m2 to R$ formula 
-	// 	result.value = Number(input.value)*200;
-
-	// }else if(option_from === "pal" && option_to==="R$"){
-	// 	//this is m2 to m2 formula 
-	// 	result.value =  Number(input.value)*50*12;
-	// }else if(option_from === "pal" && option_to==="pal"){
-	// 	//this is m2 to m2 formula 
-	// 	result.value = (Number(input.value));
-	// }
-
 
 
 }
-
