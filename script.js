bd56b09b646b3e6e44838202f79eed373100042b
ROGERIO RUFINO <92214783+RogerioRuffino@users.noreply.github.com>
2023-01-28 18:22:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const preco = document.getElementById('preco');
+const precofrete = document.getElementById('precofrete');
 
-let input = document.getElementById('input');
+
+let ent = document.getElementById('input');
 let result = document.getElementById('result');
 
 let inputType = document.getElementById('inputType');
@@ -11,49 +13,108 @@ let display1 = document.getElementById('div1').style.display;
 let display2 = document.getElementById('div2').style.display;
 
 
-// now addEventListener
-preco.addEventListener('keyup', myResult);
-input.addEventListener('keyup',myResult);
+
+
+// taking initial values
+precofrete.addEventListener('keyup', myResult );
+preco.addEventListener('keyup', myResult );
+ent.addEventListener('keyup',myResult);
 inputType.addEventListener('change',myResult);
 resultType.addEventListener('change',myResult);
 
-// taking initial values
+
 option_from = inputType.value;
 option_to = resultType.value;
 option_to = resultapetes.value;
 option_to = valor.value;
 option_to = m2.value;
+option_to = r$frete.value;
 
+
+// ========================================================
+// ========================================================
+
+function strtonum(fl){
+		
+	if(fl === ""){
+		fl =  0;
+	}
+else {
+	fl = fl.replace(".","");
+    fl = fl.replace(",",".");
+	fl = parseFloat(fl).toFixed(2);
+	 }
+	return fl;
+}
+
+function numberToBr(numero) {
+    var numero = numero.toFixed(2).split('.');
+    numero[0] =  numero[0].split(/(?=(?:...)*$)/).join('.');
+    return numero.join(',');
+}
+
+
+
+function numtostr(str,n) {
+
+var elemento = document.getElementsByTagName('input')[n];
+var valor = elemento.value;
+
+valor = valor + '';
+valor = parseInt(valor.replace(/[\D]+/g, ''));
+valor = valor + '';
+valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+if (valor.length > 6) {
+ valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");  
+}
+
+elemento.value = valor;
+if(valor == 'NaN') elemento.value = '';
+
+}
+// ============== function myResult ====================
 
 function myResult(){
+
     option_from = inputType.value;
     option_to = resultType.value;
+
+// ==============   m2 para PluginArray, tap e r$ ===
 
     if(option_from === "m2" && option_to==="pal"){
 
 		document.getElementById('div1').style.display = 'block';
         document.getElementById('div2').style.display = 'none';
+		var precoToNum = strtonum(preco.value);
+		var pfToNum = strtonum(precofrete.value);
+		var entradaToNUm = strtonum(input.value);
+		var tapetesToNum = ((entradaToNUm) % 50*4);
+		var reaisTotalToNum = (entradaToNUm * precoToNum);
 
-		result.value = (Math.floor(Number(input.value) / 50));
-
-		resultapetes.value = (Number(input.value) % 50*4).toFixed(2);
-
-		valor.value = input.value * Number(preco.value).toFixed(2);
 		
-		m2.value = Number(input.value).toFixed(2);
-		
-	} 
+		result.value = numberToBr(Math.floor(Number(entradaToNUm) / 50));
+		resultapetes.value = numberToBr(tapetesToNum);
+		valor.value = numberToBr(reaisTotalToNum);
+		r$frete.value = numberToBr(pfToNum*entradaToNUm);
+	}
+	
+// ==============   r$ para PluginArray, tap e m2 ===
 
-	//===================================================
-
-	if(option_from === "R$" && option_to==="pal"){
+	else if(option_from === "R$" && option_to==="pal"){
 		document.getElementById('div2').style.display = 'block';
     	document.getElementById('div1').style.display = 'none';
 
-		result.value = (Math.floor((Number(input.value)) / (50*Number(preco.value))));
-		resultapetes.value = (Math.floor((Number(input.value)) % ((50*Number(preco.value))))/ Number(preco.value)*4).toFixed(2);
-		valor.value = Number(input.value).toFixed(2);
-		m2.value = (Number(input.value)/12).toFixed(2);
+		var pToNum = strtonum(preco.value);
+		var pfrToNum = strtonum(precofrete.value);
+		var eToNUm = strtonum(input.value);
+		var tToNum = (((Number(eToNUm)) % ((50*pToNum))) / (pToNum)*4);
+		var m2ToNum = eToNUm/pToNum;
 		
-	}
-}
+
+		result.value = (Math.floor((eToNUm) / (50*(pToNum))));
+		resultapetes.value = numberToBr(tToNum);
+		m2.value = numberToBr(eToNUm/pToNum);
+		r$frete.value = numberToBr(m2ToNum*pfrToNum);
+		
+	}}
